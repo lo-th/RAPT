d19a98e52af62b6096a81633966dd620d0ae4ad8
lo-th <3dflashlo@gmail.com>
2015-09-04 10:28:32 +0200

    add multygun

--- src/Mine.js
+++
@@ -1,5 +0,0 @@
-var MINE = MINE || ( function () {
-    return {
-        REVISION: '0.1',
-    };
-})();
--- src/enemy/multigun.js
+++ src/enemy/multigun.js
@@ -6,7 +6,6 @@ RAPT.MULTI_GUN_RANGE = 8;
 RAPT.MultiGun = function (center) {
 	RAPT.SpawningEnemy.call(this, RAPT.ENEMY_MULTI_GUN, center, RAPT.MULTI_GUN_WIDTH, RAPT.MULTI_GUN_HEIGHT, 0, RAPT.MULTI_GUN_SHOOT_FREQ, 0);
 
-
 	this.redGun = null;
 	this.blueGun = null;
 	this.gunFired = new Array(4);
@@ -19,9 +18,20 @@ RAPT.MultiGun = function (center) {
 	this.gunPositions[1] = new RAPT.Vector(this.hitBox.getRight(), this.hitBox.getBottom());
 	this.gunPositions[2] = new RAPT.Vector(this.hitBox.getLeft(), this.hitBox.getTop());
 	this.gunPositions[3] = this.hitBox.lowerLeft.add(new RAPT.Vector(this.hitBox.getWidth(), this.hitBox.getHeight()));
+
+	this.sprite =  new RAPT.SpriteGroup({
+		name:'spikenall',
+		material:RAPT.MAT_ENEMY,
+		size : 1,
+		nuv:16,
+		list:['p1', 'p2', 'p3', 'p4'],
+		//sizes: [ [1,0.5]  ],
+		uvs:[[3,3], [4,3], [5,3], [6,3]]
+	});
+
+	this.sprite.moveto(center);
 }
 
-//RAPT.MultiGun.prototype = new RAPT.SpawningEnemy;
 RAPT.MultiGun.prototype = Object.create( RAPT.SpawningEnemy.prototype );
 
 RAPT.MultiGun.prototype.canCollide = function() {
@@ -66,73 +76,10 @@ RAPT.MultiGun.prototype.afterTick = function(seconds) {
 	this.redGun.adjustTowardsTarget(redGunTarget, speed);
 	this.blueGun.adjustTowardsTarget(blueGunTarget, speed);
 
-	//bodySprite.SetOffsetBeforeRotation(position.x, position.y);
-};
+	var angle = (this.redGun.sub(position)).atan2()-RAPT.PI90;
+	this.sprite.sprite[1].rotation.z = angle
+	this.sprite.sprite[2].rotation.z = angle;
 
-RAPT.MultiGun.prototype.draw = function(c) {
-	// Draw the red and/or blue circles
-	if (this.redGun.eq(this.blueGun) && !RAPT.gameState.playerA.isDead() && !RAPT.gameState.playerB.isDead()) {
-		var angle = (this.redGun.sub(this.getCenter())).atan2();
-		c.fillStyle = "rgb(205, 0, 0)";
-		c.beginPath();
-		c.arc(this.redGun.x, this.redGun.y, 0.1, angle, angle + Math.PI, false);
-		c.fill();
-		c.fillStyle = "rgb(0, 0, 255)";
-		c.beginPath();
-		c.arc(this.blueGun.x, this.blueGun.y, 0.1, angle + Math.PI, angle + 2 * Math.PI, false);
-		c.fill();
-	} else {
-		if (!RAPT.gameState.playerA.isDead()) {
-			c.fillStyle = "rgb(205, 0, 0)";
-			c.beginPath();
-			c.arc(this.redGun.x, this.redGun.y, 0.1, 0, 2 * Math.PI, false);
-			c.fill();
-		}
-		if(!RAPT.gameState.playerB.isDead()) {
-			c.fillStyle = "rgb(0, 0, 255)";
-			c.beginPath();
-			c.arc(this.blueGun.x, this.blueGun.y, 0.1, 0, 2 * Math.PI, false);
-			c.fill();
-		}
-	}
-
-	// Draw the body
-	c.strokeStyle = "black";
-	c.beginPath();
-	// Bottom horizontal
-	c.moveTo(this.gunPositions[0].x, this.gunPositions[0].y + 0.1);
-	c.lineTo(this.gunPositions[1].x, this.gunPositions[1].y + 0.1);
-	c.moveTo(this.gunPositions[0].x, this.gunPositions[0].y - 0.1);
-	c.lineTo(this.gunPositions[1].x, this.gunPositions[1].y - 0.1);
-	// Top horizontal
-	c.moveTo(this.gunPositions[2].x, this.gunPositions[2].y - 0.1);
-	c.lineTo(this.gunPositions[3].x, this.gunPositions[3].y - 0.1);
-	c.moveTo(this.gunPositions[2].x, this.gunPositions[2].y + 0.1);
-	c.lineTo(this.gunPositions[3].x, this.gunPositions[3].y + 0.1);
-	// Left vertical
-	c.moveTo(this.gunPositions[0].x + 0.1, this.gunPositions[0].y);
-	c.lineTo(this.gunPositions[2].x + 0.1, this.gunPositions[2].y);
-	c.moveTo(this.gunPositions[0].x - 0.1, this.gunPositions[0].y);
-	c.lineTo(this.gunPositions[2].x - 0.1, this.gunPositions[2].y);
-	// Right vertical
-	c.moveTo(this.gunPositions[1].x - 0.1, this.gunPositions[1].y);
-	c.lineTo(this.gunPositions[3].x - 0.1, this.gunPositions[3].y);
-	c.moveTo(this.gunPositions[1].x + 0.1, this.gunPositions[1].y);
-	c.lineTo(this.gunPositions[3].x + 0.1, this.gunPositions[3].y);
-	c.stroke();
-
-	// Draw the gun holders
-	c.beginPath();
-	c.arc(this.gunPositions[0].x, this.gunPositions[0].y, 0.1, 0, 2 * Math.PI, false);
-	c.stroke();
-	c.beginPath();
-	c.arc(this.gunPositions[1].x, this.gunPositions[1].y, 0.1, 0, 2 * Math.PI, false);
-	c.stroke();
-	c.beginPath();
-	c.arc(this.gunPositions[2].x, this.gunPositions[2].y, 0.1, 0, 2 * Math.PI, false);
-	c.stroke();
-	c.beginPath();
-	c.arc(this.gunPositions[3].x, this.gunPositions[3].y, 0.1, 0, 2 * Math.PI, false);
-	c.stroke();
-
+	this.sprite.sprite[1].position.set(this.redGun.x-position.x, this.redGun.y-position.y,0);
+	this.sprite.sprite[2].position.set(this.blueGun.x-position.x, this.blueGun.y-position.y,0);
 };
